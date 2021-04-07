f84737cb7fa4df60bf8781020a128f76129df77b
Harrison King <authorharrisonking@gmail.com>
2021-04-07 16:40:06 -0600

    Deploy app to Heroku

---
+++ Server/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!"});
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+//look for templates you can model it after, look at an open source option.
+
+/* 
+https://harrison-king-portfolio.herokuapp.com/
+to update site:
+
+git add .
+git commit -m "my commit message"
+git push heroku master
+*/
