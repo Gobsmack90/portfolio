a2a1067f0065eef9e7e11d4ab709ea5d109f3f66
Harrison King <authorharrisonking@gmail.com>
2021-04-30 15:55:04 -0600

    scrubbed personal information with other fixes

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -5,11 +5,8 @@ class Contact extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
+      var email= this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
@@ -77,11 +74,10 @@ class Contact extends Component {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
+					   <h4>Or contact me at</h4>
 					   <p className="address">
 						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
+						   {email}<br />
 						   <span>{phone}</span>
 					   </p>
 				   </div>
